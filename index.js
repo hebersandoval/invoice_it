03ec86d816090ab5e909aa909471732cf8655504
Heber Sandoval <he3ers@gmail.com>
2025-12-06 08:07:54 -0600

    Server can listen to a few endpoints and respond with simple content.

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const http = require('http');
 // Create server
 const server = http.createServer((request, response) => {
     // request - from client (browser) to server
+    // response - from server to client
     console.log(request.url);
 
-    // response - from server to client
-    response.end('Hello browser!');
+    if (request.url === '/about') {
+        return response.end('About page');
+    } else if (request.url === '/contact') {
+        return response.end('Contact page');
+    } else if (request.url == '/') {
+        return response.end('Blog it!');
+    } else {
+        response.writeHead(404);
+
+        response.end('Page not found');
+    }
 });
 
 server.listen(3000);
