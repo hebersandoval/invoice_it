259f1be35f20254dc8a1ccf69fe54f9899660ac8
Heber Sandoval <he3ers@gmail.com>
2025-12-12 11:37:12 -0600

    Cleanup the user router routes.

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,17 @@ const router = express.Router();
 
 const { getUser, createUser, deleteUser } = require('../controllers/user.controller');
 
-router.get('/', getUser);
-router.get('/create', createUser);
-router.get('/delete', deleteUser);
+// User routes
+router.get('/', (request, response) => {
+    response.render('pages/index', { title: 'Invoice it!' });
+});
+
+router.get('/login', (request, response) => {
+    response.render('pages/login', { title: 'Sign in' });
+});
+
+router.get('/signup', (request, response) => {
+    response.render('pages/signup', { title: 'Sign up' });
+});
 
 module.exports = router;
