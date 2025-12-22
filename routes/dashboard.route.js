62074344c98c85a77339be280da1a472a1b01d60
Heber Sandoval <he3ers@gmail.com>
2025-12-21 19:39:17 -0600

    Able to get the 'info' value from flash().

--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-    response.render('pages/dashboard', { title: 'Dashboard' });
+    response.render('pages/dashboard', {
+        title: 'Dashboard',
+        info: request.flash('info')[0],
+    });
 });
 
 module.exports = router;
