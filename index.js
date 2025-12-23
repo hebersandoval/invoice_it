dcf41627092591ab071f4a411bd43e3366c14d5a
Heber Sandoval <he3ers@gmail.com>
2025-12-23 09:04:38 -0600

    Cleaned up templating engine for hbs.

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 
 require('dotenv').config({ debug: true });
-require('./libs/dbConnect');
 
-// const { engine } = require('express-handlebars');
+require('./libs/dbConnect');
 
 const userRouter = require('./routes/user.route');
 
@@ -38,18 +37,6 @@ app.use(flash());
 // Sets view engine
 app.set('views', `${__dirname}/views`);
 
-/*
-app.engine(
-    '.hbs',
-    engine({
-        extname: 'hbs', // .handlebars
-        layoutsDir: path.join(__dirname, 'views/layouts'),
-        partialsDir: path.join(__dirname, 'views/partials'),
-        defaultLayout: 'main.hbs',
-    })
-);
-*/
-
 app.set('view engine', '.ejs');
 
 // Routes
@@ -61,6 +48,7 @@ app.use('/dashboard', verifyUser, dashoardRouter);
 app.use((request, response) => {
     response.status(404).render('index', { message: 'Page not found', title: 'Lost?' });
 });
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
