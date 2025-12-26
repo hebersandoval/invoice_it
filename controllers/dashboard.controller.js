afca28b452d94622d3fe98ada0d4bd8fa23b4e75
Heber Sandoval <he3ers@gmail.com>
2025-12-26 09:53:20 -0600

    Gets total count of invoices and customers and find all associated customer's invoices.

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -4,7 +4,17 @@ const Invoice = require('../libs/models/invoice.model');
 const { USDollar } = require('../libs/formatter');
 
 const showDashboard = async (request, response) => {
-    //
+    const query = { owner: request.session.userId };
+
+    // Get total count of invoices and customers
+    const invoiceCount = await Invoice.countDocuments(query);
+    const customerCount = await Customer.countDocuments(query);
+
+    const allInvoices = await Invoice.find(query).populate({
+        path: 'customer',
+        model: Customer,
+        select: '_id name',
+    });
 };
 
 module.exports = {
