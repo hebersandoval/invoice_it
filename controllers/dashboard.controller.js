7b92a8e8a9b18a80dd0cd65c096710638c7adb90
Heber Sandoval <he3ers@gmail.com>
2025-12-26 09:46:40 -0600

    Create a dashboard controller function to show some stats.

---
+++ controllers/dashboard.controller.js
@@ -0,0 +1,12 @@
+const Customer = require('../libs/models/customer.model');
+const Invoice = require('../libs/models/invoice.model');
+
+const { USDollar } = require('../libs/formatter');
+
+const showDashboard = async (request, response) => {
+    //
+};
+
+module.exports = {
+    showDashboard,
+};
